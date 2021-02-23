bbb19bbe42c448aadba058ce1c4511eb6ffcb16b
danielburgosr96 <danielburgosr96@gmail.com>
2021-02-23 11:37:33 -0500

    Todo entity created

--- crud/src/main/java/co/com/sofka/crud/Todo.java
+++ crud/src/main/java/co/com/sofka/crud/Todo.java
@@ -1,5 +1,40 @@
 package co.com.sofka.crud;
 
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Todo {
+
+    @Id
+    @GeneratedValue
+    private Float id;
+    private String name;
+    private boolean isCompleted;
+
+    public Float getId() {
+        return id;
+    }
+
+    public void setId(Float id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+
+    public void setCompleted(boolean completed) {
+        isCompleted = completed;
+    }
 }
